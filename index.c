3d87990ded2ef01c51715bf31ec6c033f250e2f8
azim-hoss <azimhossainahs@gmail.com>
2024-07-29 21:34:18 +0600

    hvh

---
+++ index.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+    int main(){
+        char x[]="Azim Hossain";
+        
+        printf("%s\nstrlen =%d\nsizeof =%d ",x , strlen(x), sizeof(x));
+
+        return 0;
+    }
