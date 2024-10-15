1690b8e8aebc2d990dc578021882d6bc0fcb6265
azim <160012980+azimhossainahs@users.noreply.github.com>
2024-10-15 20:53:21 +0600

    Delete index.c

--- index.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-    int main(){
-        char x[]="Azim Hossain";
-        
-        printf("%s\nstrlen =%d\nsizeof =%d ",x , strlen(x), sizeof(x));
-
-        return 0;
-    }
